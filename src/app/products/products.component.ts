0b3190cd07de35b154f9fbb26e03bda8cdc885e1
Fatih Delice <delicex58@gmail.com>
2023-01-29 00:53:13 +0300

    Routing orientation

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-products',
@@ -6,5 +7,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./products.component.css']
 })
 export class ProductsComponent {
+  products = [
+    {name: 'product 1'},
+    {name: 'product 2'},
+    {name: 'product 3'}
+  ];
 
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute
+    ) { }
+
+  loadProducts() {
+    this.router.navigate(['/products'], {relativeTo: this.route});
+  }
 }
