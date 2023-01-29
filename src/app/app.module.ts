e6b2a07aedbabaf6a1c9dcb07e8e16a7dee8f0b3
Fatih Delice <delicex58@gmail.com>
2023-01-29 18:31:41 +0300

    App routing module added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,24 +11,10 @@ import { NotfoundComponent } from './notfound/notfound.component';
 import { ProductComponent } from './products/product/product.component';
 import { UserComponent } from './users/user/user.component';
 import { EditProductsComponent } from './products/edit-products/edit-products.component';
+import { AppRoutingModule } from './app-routing.module';
 
 
-const appRoutes: Routes = [
-  { path: '', component: HomeComponent },
-  { path: 'home', component: HomeComponent },
-  {
-    path: 'products', component: ProductsComponent, children: [
-      { path: ':id', component: ProductComponent },
-      { path: ':id/edit', component: EditProductsComponent }
-    ]
-  },
-  {
-    path: 'users', component: UsersComponent, children: [
-      { path: ':name', component: UserComponent }
-    ]
-  },
-  { path: '**', component: NotfoundComponent },
-];
+
 
 // localhost:4200/products?page=2&order=newest
 // query params
@@ -47,7 +33,7 @@ const appRoutes: Routes = [
   ],
   imports: [
     BrowserModule,
-    RouterModule.forRoot(appRoutes)
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
